a7d26c9a1ed3b9f2d9ecc2ec4ac63c32459e20c4
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-10-22 01:04:12 +0300

    Added all pokemons requests

---
+++ src/routers/pokemonRouter.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const router = express.Router();
+const Pokedex = require('pokedex-promise-v2');
+const P = new Pokedex();
+const isUsername = require('../middleware/userHandler');
+const errorHandler = require('../middleware/errorHandler');
+const derectives = require('../derectives');
+
+//  Check if request have header {username : <string>}
+router.use(isUsername);
+
+// Get request by name in query params
+router.get('/query', (req, res) => {
+  const name = req.query.name;
+  P.getPokemonByName(name)
+    .then((result) => {
+      const pokemon = derectives.generationPokemonObject(result);
+      res.send(pokemon);
+      res.end();
+    })
+    .catch((err) => {
+      res.status(404).json({ Error: `${err.message}. Pokemon not found.` });
+      res.end();
+    });
+});
+
+// Get request by id url params
+router.get('/get/:id', (req, res) => {
+  const { id } = req.params;
+  P.getPokemonByName(id)
+    .then((result) => {
+      const pokemon = derectives.generationPokemonObject(result);
+      res.send(pokemon);
+      res.end();
+    })
+    .catch((err) => {
+      res.status(404).json({ Error: `${err.message}. Pokemon not found.` });
+      res.end();
+    });
+});
+
+//Get request that show all pokemons from DataBase of the user
+router.get('/', (req, res) => {
+  try {
+    const username = req.headers.username;
+    const allUsernamePokemons = derectives.listYoursPokemons(username);
+    res.send(allUsernamePokemons);
+    res.end();
+  } catch {
+    throw Error;
+  }
+});
+
+//Put request of catching pokemon and creating file in DataBase
+router.put('/catch/:id', errorHandler.isPokemonCatched, (req, res) => {
+  const { id } = req.params;
+  P.getPokemonByName(id)
+    .then((result) => {
+      const pokemon = derectives.generationPokemonObject(result);
+      derectives.createPokemonFile(pokemon, id, req.headers.username);
+      res.send('catch');
+      res.end();
+    })
+    .catch((err) => {
+      res.status(404).json({ Error: `${err.message}. Pokemon not found.` });
+      res.end();
+    });
+});
+
+//Delete request of deleting caught pokemon and deleting file from DataBase
+router.delete('/release/:id', errorHandler.isPokemonCatched, (req, res) => {
+  try {
+    const { id } = req.params;
+    derectives.deletePokemonFile(id, req.headers.username);
+    res.send('succed');
+    res.end();
+  } catch {
+    throw Error;
+  }
+});
+
+module.exports = router;
