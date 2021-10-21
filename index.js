e6d708845955025b83cafbf8cbdaaebc70afae5d
DimaTomilin <89575657+DimaTomilin@users.noreply.github.com>
2021-10-22 01:05:06 +0300

    Connecting all routers

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+const pokemonRouter = require('./src/routers/pokemonRouter');
+const userRouter = require('./src/routers/userRouter');
+const errorHandler = require('./src/middleware/errorHandler');
+
 const port = 8080;
 
-// start the server
-app.listen(port, function() {
-    console.log('app started');
-  });
-
-// route our app
-app.get('/', function(req, res) {
-  res.send('hello world!');
+app.listen(port, function () {
+  console.log('app started');
 });
+
+app.use('/pokemon', pokemonRouter);
+app.use('/users', userRouter);
+app.use(errorHandler.serverError);
